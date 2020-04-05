e5c9ea1107e8578e93e49d69358b3ac3ff3264df
Jared Dunbar <jrddunbr@gmail.com>
2020-04-05 15:43:53 -0400

    added a bunch of rendering code to handle player movement better

--- main.py
+++ main.py
@@ -75,13 +75,21 @@ class Drawn():
                 pyxel.image(self.bank).load(self.xLoc, self.yLoc, texture)
                 texture8[name] = self
 
-    def draw(self, x, y):
-        if self.bank == 0:
-            # Bank 0. Draw 16x16
-            pyxel.blt(x*16, y*16, self.bank, self.xLoc, self.yLoc, 16, 16, self.trans)
-        elif  self.bank == 1:
-            # Bank 1: Draw 8x8
-            pyxel.blt(x*8, y*8, self.bank, self.xLoc, self.yLoc, 8, 8, self.trans)
+    def draw(self, x, y, trans=None, fX=False, fY=False):
+        if (trans == None):
+            trans = self.trans
+        # Default texture size is 16x16
+        ts = 16
+        # If we're in Bank 1, texture size is 8x8
+        if self.bank == 1:
+            ts = 8
+        xf = ts
+        yf = ts
+        if fX:
+            xf = -ts
+        if fY:
+            yf = -ts
+        pyxel.blt(x*abs(ts), y*abs(ts), self.bank, self.xLoc, self.yLoc, xf, yf, trans)
 
 class Sounded():
     def __init__(self, name, notes, tone="s", volume="4", effect=("n" * 4 + "f"), speed=7):
@@ -96,13 +104,17 @@ class Sounded():
 
 # This is the base class of any thing that renders to the screen and ticks.
 class Entity():
-    def __init__(self, name, texture="invalid16.png", x=0, y=0):
+    def __init__(self, name, texture=["invalid16.png"], x=0, y=0):
         self.name = name
         self.x = x
         self.y = y
         self.allow = False
-        self.texName = texture.rsplit(".",1)[0] # remove file extension
-        Drawn(self.texName, 16, texture)
+        self.frameNum = 0
+        self.dir = "N"
+        self.texName = [x.rsplit(".",1)[0] for x in texture]
+        for tex in texture:
+            texName = tex.rsplit(".",1)[0] # remove file extension
+            Drawn(texName, 16, tex)
 
     def update(self):
         pass
@@ -111,19 +123,22 @@ class Entity():
         drawX = self.x + windowOffsetX
         drawY = self.y + windowOffsetY
         if (drawX >= 0 and drawX < WIDTH) and (drawY >=0 and drawY < HEIGHT):
-            texture16[self.texName].draw(drawX, drawY)
+            texture16[self.texName[self.frameNum]].draw(drawX, drawY)
 
 class Floor(Entity):
     def __init__(self, name, x, y):
-        super(Floor, self).__init__(name, "floor.png", x, y)
+        super(Floor, self).__init__(name, ["floor.png"], x, y)
         self.allow = True
 
 # The player class extends Entity by listening for keyboard events.
 class Player(Entity):
     def __init__(self, name, x=WIDTH/2, y=HEIGHT/2):
-        super(Player, self).__init__(name, "player.png", x, y)
+        super(Player, self).__init__(name, ["player/char_H{}.png".format(x) for x in range(0,12)] + ["player/char_V{}.png".format(x) for x in range(0,12)], x, y)
         self.cooldown = 0
         self.cooldownTime = 2
+        self.frameNum = 1
+        self.texHnames = [x for x in self.texName if "H" in x]
+        self.texVnames = [x for x in self.texName if "V" in x]
 
     def update(self):
         self.cooldown -= 1
@@ -132,12 +147,16 @@ class Player(Entity):
             wantGoY = 0
             if pyxel.btn(pyxel.KEY_UP):
                 wantGoY -= 1
+                self.dir = "N"
             if pyxel.btn(pyxel.KEY_DOWN):
                 wantGoY += 1
+                self.dir = "S"
             if pyxel.btn(pyxel.KEY_LEFT):
                 wantGoX -= 1
+                self.dir = "E"
             if pyxel.btn(pyxel.KEY_RIGHT):
                 wantGoX += 1
+                self.dir = "W"
 
             if (wantGoX != 0 or wantGoY != 0):
                 if canGo(self.x, self.y, wantGoX, wantGoY):
@@ -148,6 +167,39 @@ class Player(Entity):
                     self.cooldown = self.cooldownTime
                     windowOffsetX -= wantGoX
                     windowOffsetY -= wantGoY
+
+    def draw(self):
+        drawX = self.x + windowOffsetX
+        drawY = self.y + windowOffsetY
+
+        fX = False
+        fY = False
+        ch = self.texHnames
+
+        if self.dir == "N":
+            fX = True
+            fY = True
+            ch = self.texVnames
+        if self.dir == "S":
+            fX = False
+            fY = False
+            ch = self.texVnames
+        if self.dir == "E":
+            fX = False
+            fY = False
+            ch = self.texHnames
+        if self.dir == "W":
+            fX = True
+            fY = True
+            ch = self.texHnames
+
+        print(ch)
+
+        if (drawX >= 0 and drawX < WIDTH) and (drawY >=0 and drawY < HEIGHT):
+            texture16[ch[self.frameNum - 1]].draw(drawX, drawY, 0, fX=fX, fY=fY)
+        self.frameNum += 1
+        if (self.frameNum >= 12):
+            self.frameNum = 0
 
 # This tells you if an entity is permitted to go somewhere.
 # From x,y with velocity a,b
@@ -186,7 +238,7 @@ def canGo(x, y, a, b):
 # This sets up the game
 def setup():
     # Register with Pyxel
-    pyxel.init(WIDTH * 16, HEIGHT * 16, caption="smolgame", scale=4, fps=20)
+    pyxel.init(WIDTH * 16, HEIGHT * 16, caption="smolgame", palette=[0xff00e5, 0xaaa9ad, 0x5b676d, 0x1f262a, 0x9cff78, 0x44ff00, 0x2ca600, 0x7cff00, 0xff8b00, 0xff0086, 0x6f00ff, 0x0086ff, 0x00ff9a, 0x1f0000, 0x49afff, 0xe2e1ff], scale=4, fps=20)
 
     # Register sounds
     Sounded("collide", "c2c1", speed=4)
@@ -206,7 +258,7 @@ def worldgen():
     for a in range(0, 64):
         x = random.randrange(0, GL_WIDTH - 1)
         y = random.randrange(0, GL_HEIGHT - 1)
-        wall = Entity("wall", "wall.png", x, y)
+        wall = Entity("wall", ["wall.png"], x, y)
         structures.append(wall)
 
     # last step: Make the floor. NOTE: This alg is terrible and is O(n^3) at worst.
